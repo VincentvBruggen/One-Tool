90cbfe5c7ac9a90b120bb7a4840d1afba59a48c5
VincentvBruggen <lachenbekkie2701@gmail.com>
2024-09-26 16:19:25 +0200

    fixed worldGen

---
+++ Assets/_Vincent/Scripts/ToolManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolManager : MonoBehaviour
+{
+    public int toolLevel;
+    public SpriteRenderer toolVisual;
+
+    [SerializeField] Sprite[] toolsList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
