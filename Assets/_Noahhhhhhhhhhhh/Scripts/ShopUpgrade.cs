a3cfa769341edf4746881e68f40cb9e4919d52b6
Linkerpink <noahvanuunen2007@gmail.com>
2024-09-27 10:01:10 +0200

    NU alles pushen

--- Assets/_Noahhhhhhhhhhhh/Scripts/ShopUpgrade.cs
+++ Assets/_Noahhhhhhhhhhhh/Scripts/ShopUpgrade.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShopUpgrade : MonoBehaviour
 {
-    [SerializeField] private Inventory inventory;
+    private Inventory inventory;
 
-    [SerializeField] private ToolManager toolManager;
+    private ToolManager toolManager;
 
     [SerializeField] private int[] upgradeCost;
 
-    private int playerLevel;
-    
+    [SerializeField] private TextMeshProUGUI mainText;
+    [SerializeField] private TextMeshProUGUI secondText;
+
+    private void Awake()
+    {
+        inventory = GameObject.Find("Player").GetComponent<Inventory>();
+        toolManager = GameObject.Find("Player").GetComponent<ToolManager>();
+    }
+
+    private void Update()
+    {
+        mainText.SetText("Upgrade to lvl " + toolManager.toolLevel + 1);
+        secondText.SetText("Cost: " + upgradeCost[toolManager.toolLevel + 1]);
+    }
+
     public void UpgradeTool()
     {
-        if (inventory.currency >= upgradeCost[toolManager.toolLevel])
+        if (inventory.currency >= upgradeCost[toolManager.toolLevel + 1] && toolManager.toolLevel < 5)
         {
-            inventory.currency -= upgradeCost[toolManager.toolLevel];
+            inventory.currency -= upgradeCost[toolManager.toolLevel + 1];
             toolManager.toolLevel++;
         }
     }
